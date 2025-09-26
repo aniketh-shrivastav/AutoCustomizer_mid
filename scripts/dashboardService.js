2caae3630b617c51e81c6cb1a17ad31b095efed4
Prathiba-Ravi <prathibar401@gmail.com>
2025-09-26 22:52:51 +0530

    sercvice dashboard update

--- scripts/dashboardService.js
+++ scripts/dashboardService.js
@@ -95,9 +95,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
   
-
-
-function toggleSidebar() {
-  const sidebar = document.getElementById("sidebar");
-  sidebar.classList.toggle("active");
-}
